2b56f5c67133a3d85a74866b1fdd1e24a7ba65f2
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-08-03 18:17:31 +0900

    test: add test case of close

--- src/file_system/file_system_sync_access_handle_test.ts
+++ src/file_system/file_system_sync_access_handle_test.ts
@@ -1,16 +1,16 @@
 import { expect } from "@std/expect";
 import { beforeEach, describe, it } from "@std/testing/bdd";
-import { createEmptyFile } from "@test";
+import { FileSystemSyncAccessHandle } from "./file_system_sync_access_handle.ts";
 import { FileSystemDirectoryHandle } from "./file_system_directory_handle.ts";
 import { define } from "./helper.ts";
 
 interface Context {
-  root: FileSystemDirectoryHandle;
+  handle: FileSystemSyncAccessHandle;
 }
 
 describe("FileSystemSyncAccessHandle", () => {
-  beforeEach<Context>(function () {
-    this.root = new FileSystemDirectoryHandle(
+  beforeEach<Context>(async function () {
+    const root = new FileSystemDirectoryHandle(
       { kind: "directory", path: [""], root: "" },
       define({
         getBinaryData() {
@@ -30,32 +30,102 @@ describe("FileSystemSyncAccessHandle", () => {
         },
       }),
     );
+
+    const handle = await root.getFileHandle("file.txt", { create: true });
+
+    this.handle = await handle.createSyncAccessHandle();
   });
 
   describe("getSize", () => {
     it<Context>(
       "test SyncAccessHandle.getSize after SyncAccessHandle.write",
-      async function () {
-        const handle = await createEmptyFile(this.root, "file.txt");
-        const writer = await handle.createSyncAccessHandle();
-
-        expect(writer.getSize()).toBe(0);
+      function () {
+        expect(this.handle.getSize()).toBe(0);
 
         const bufferSize = 4;
         const writeBuffer = new Uint8Array(bufferSize);
         writeBuffer.set([96, 97, 98, 99]);
 
-        writer.write(writeBuffer, { at: 0 });
+        this.handle.write(writeBuffer, { at: 0 });
 
-        expect(writer.getSize()).toBe(bufferSize);
+        expect(this.handle.getSize()).toBe(bufferSize);
 
         let offset = 3;
-        writer.write(writeBuffer, { at: offset });
-        expect(writer.getSize()).toBe(bufferSize + offset);
+        this.handle.write(writeBuffer, { at: offset });
+        expect(this.handle.getSize()).toBe(bufferSize + offset);
 
         offset = 10;
-        writer.write(writeBuffer, { at: offset });
-        expect(writer.getSize()).toBe(bufferSize + offset);
+        this.handle.write(writeBuffer, { at: offset });
+        expect(this.handle.getSize()).toBe(bufferSize + offset);
+      },
+    );
+  });
+
+  describe("close", () => {
+    it<Context>(
+      "SyncAccessHandle.close is idempotent",
+      function () {
+        expect(this.handle.close()).toBe(undefined);
+        expect(this.handle.close()).toBe(undefined);
+      },
+    );
+
+    it<Context>(
+      "SyncAccessHandle.read fails after SyncAccessHandle.close",
+      function () {
+        expect(this.handle.close()).toBe(undefined);
+
+        const readBuffer = new Uint8Array(4);
+
+        expect(() => this.handle.read(readBuffer, { at: 0 })).toThrow(
+          DOMException,
+        );
+      },
+    );
+
+    it<Context>(
+      "SyncAccessHandle.write fails after SyncAccessHandle.close",
+      function () {
+        expect(this.handle.close()).toBe(undefined);
+        const writeBuffer = new Uint8Array(4);
+        writeBuffer.set([96, 97, 98, 99]);
+
+        expect(() => this.handle.write(writeBuffer, { at: 0 })).toThrow(
+          DOMException,
+        );
+      },
+    );
+
+    it<Context>(
+      "SyncAccessHandle.flush fails after SyncAccessHandle.close",
+      function () {
+        expect(this.handle.close()).toBe(undefined);
+
+        expect(() => this.handle.flush()).toThrow(
+          DOMException,
+        );
+      },
+    );
+
+    it<Context>(
+      "SyncAccessHandle.getSize fails after SyncAccessHandle.close",
+      function () {
+        expect(this.handle.close()).toBe(undefined);
+
+        expect(() => this.handle.getSize()).toThrow(
+          DOMException,
+        );
+      },
+    );
+
+    it<Context>(
+      "SyncAccessHandle.truncate fails after SyncAccessHandle.handle.close",
+      function () {
+        expect(this.handle.close()).toBe(undefined);
+
+        expect(() => this.handle.truncate(4)).toThrow(
+          DOMException,
+        );
       },
     );
   });
