e9aa6cd467e51f8515d647d4ddce2833d590250b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-08-20 17:46:24 +0900

    feat(mod): export all modules defined in specification

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,13 @@
-export * from "./type.ts";
 export { createFileSystemHandle } from "./algorithm.ts";
 export * from "./file_system_directory_handle.ts";
 export * from "./file_system_file_handle.ts";
 export * from "./file_system_handle.ts";
 export * from "./file_system_observer.ts";
+export * from "./file_system.ts";
+export * from "./file_system_change_record.ts";
+export * from "./file_system_entry.ts";
+export * from "./file_system_locator.ts";
+export * from "./file_system_sync_access_handle.ts";
+export * from "./file_system_writable_file_stream.ts";
 export * from "./symbol.ts";
-export { StorageManager } from "./storage_manager.ts";
+export * from "./storage_manager.ts";
