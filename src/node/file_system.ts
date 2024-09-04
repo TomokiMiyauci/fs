1ad769bb2048bd96b7f6a59a686829074e2f5876
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-04 23:10:20 +0900

    docs(node): add description

--- src/node/file_system.ts
+++ src/node/file_system.ts
@@ -29,13 +29,29 @@ import {
 } from "@miyauci/fs";
 import { isDirectoryEntry } from "../algorithm.ts";
 
+/** {@link IFileSystem File system} for Node.js runtime. */
 export class FileSystem implements IFileSystem {
+  /**
+   * Construct {@link FileSystem}.
+   *
+   * @param root Absolute path.
+   *
+   * @example Basic
+   * ```ts
+   * import { FileSystem } from "@miyauci/fs/node";
+   * const fs = new FileSystem("/path/to/dir");
+   * ```
+   */
   constructor(root: string) {
     this.root = root;
   }
 
+  /** Root directory path. */
   root: string;
 
+  /**
+   * Locale {@link FileSystemEntry entry} from {@link FileSystemPath path}.
+   */
   locateEntry(path: FileSystemPath): FileSystemEntry | null {
     const fullPath = join(this.root, ...path);
 
@@ -54,20 +70,64 @@ export class FileSystem implements IFileSystem {
     }
   }
 
+  /** Set of observing {@link FileSystemObservation}. */
   observations: Set<FileSystemObservation> = new Set();
 }
 
+/**
+ * Local file system manager.
+ *
+ * @example Basic
+ * ```ts
+ * import { LocalFileSystem } from "@miyauci/fs/node";
+ *
+ * const fs = new LocalFileSystem();
+ * const handle = await fs.getDirectory();
+ * ```
+ *
+ * @example Watching directory changes
+ * ```ts
+ * import { LocalFileSystem } from "@miyauci/fs/node";
+ * const fs = new LocalFileSystem();
+ *
+ * fs.watch();
+ * ```
+ */
 export class LocalFileSystem extends FileSystem {
+  /**
+   * If {@link root} is not specified, root becomes the current working directory.
+   *
+   * @example Absolute path
+   * ```ts
+   * import { LocalFileSystem } from "@miyauci/fs/node";
+   *
+   * const fs = new LocalFileSystem("/path/to/dir");
+   * ```
+   *
+   * @example Relative path
+   * ```ts
+   * import { LocalFileSystem } from "@miyauci/fs/node";
+   *
+   * const fs = new LocalFileSystem("../");
+   * ```
+   */
   constructor(root: string = "") {
     super(resolve(root));
   }
 
+  /**
+   * Returns the root directory of the local file system.
+   */
   getDirectory(): Promise<FileSystemDirectoryHandle> {
     return Promise.resolve(
       createNewFileSystemDirectoryHandle(this, new List([""])),
     );
   }
 
+  /** Start to watch {@link root} directory.
+   *
+   * If {@link watch} has already been called, nothing is done.
+   */
   watch(): void {
     if (this.#watcher) return;
 
@@ -84,15 +144,15 @@ export class LocalFileSystem extends FileSystem {
 
   #watcher: FSWatcher | undefined;
 
+  /** End to watch {@link root} directory.
+   *
+   * If {@link watch} is not called, nothing is done.
+   */
   async unwatch(): Promise<void> {
     if (this.#watcher) {
       await this.#watcher.close();
       this.#watcher = undefined;
     }
-  }
-
-  [Symbol.asyncDispose](): Promise<void> {
-    return this.unwatch();
   }
 }
 
@@ -181,12 +241,11 @@ class DirectoryEntry extends BaseEntry implements IDirectoryEntry {
   }
 }
 
-class Effector
-  implements
-    Pick<
-      Set<FileSystemEntry>,
-      "append" | "remove" | "isEmpty" | typeof Symbol.iterator
-    > {
+class Effector implements
+  Pick<
+    Set<FileSystemEntry>,
+    "append" | "remove" | "isEmpty" | typeof Symbol.iterator
+  > {
   constructor(private root: string, private path: string[]) {}
 
   append(entry: FileSystemEntry): void {
--- src/node/mod.ts
+++ src/node/mod.ts
@@ -1,5 +1,30 @@
 /** File system related modules for Node.js runtime.
  *
+ * @example Basic
+ * ```ts
+ * import { LocalFileSystem } from "@miyauci/fs/node";
+ * const fs = new LocalFileSystem();
+ * const handle = await fs.getDirectory();
+ * const fileHandle = await handle.getFileHandle("file.txt", { create: true });
+ * const file = await fileHandle.getFile();
+ * const contents = await file.text();
+ * ```
+ *
+ * @example With FileSystemObserver
+ * ```ts
+ * import {
+ *   FileSystemObserver,
+ *   type FileSystemObserverCallback,
+ * } from "@miyauci/fs";
+ * import { LocalFileSystem } from "@miyauci/fs/node";
+ * declare const callback: FileSystemObserverCallback;
+ * const fs = new LocalFileSystem();
+ * const handle = await fs.getDirectory();
+ * const observer = new FileSystemObserver(callback);
+ * fs.watch();
+ * await observer.observe(handle);
+ * ```
+ *
  * @module
  */
 
