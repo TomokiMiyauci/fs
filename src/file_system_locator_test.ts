4a5a771a10051287251dc030c3988d8091ef37c8
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-07 17:26:10 +0900

    test: add test case

--- src/file_system_locator_test.ts
+++ src/file_system_locator_test.ts
@@ -1,35 +1,39 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import { List, Set } from "@miyauci/infra";
-import { getLocator } from "./file_system_locator.ts";
-import type { FileSystem } from "./file_system.ts";
+import { getLocator, getRelationship } from "./file_system_locator.ts";
+import type {
+  FileSystem as IFileSystem,
+  FileSystemObservation,
+} from "./file_system.ts";
 import type {
   DirectoryEntry,
   FileEntry,
   FileSystemEntry,
 } from "./file_system_entry.ts";
 
+class FileSystem implements IFileSystem {
+  getPath(entry: FileSystemEntry) {
+    const path = new List([entry.name]);
+    let parent = entry.parent;
+
+    while (parent) {
+      path.prepend(parent.name);
+
+      parent = parent.parent;
+    }
+
+    return path;
+  }
+  locateEntry() {
+    return null;
+  }
+  root: string = "";
+  observations: Set<FileSystemObservation> = new Set();
+}
+
 describe("getLocator", () => {
-  const fileSystem = {
-    getPath(entry) {
-      const path = new List([entry.name]);
-      let parent = entry.parent;
-
-      while (parent) {
-        path.prepend(parent.name);
-
-        parent = parent.parent;
-      }
-
-      return path;
-    },
-    locateEntry() {
-      return null;
-    },
-    root: "",
-    observations: new Set(),
-  } satisfies FileSystem;
-
+  const fileSystem = new FileSystem();
   const baseEntry = {
     fileSystem,
     queryAccess() {
@@ -113,5 +117,81 @@ describe("getLocator", () => {
       path: new List([dirName, dirName]),
       kind: "directory",
     });
+  });
+});
+
+describe("getRelationship", () => {
+  it("should return 'other' if fileSystem is not same", () => {
+    expect(
+      getRelationship({
+        fileSystem: new FileSystem(),
+        kind: "file",
+        path: new List(),
+      }, {
+        fileSystem: new FileSystem(),
+        kind: "file",
+        path: new List(),
+      }),
+    ).toBe("other");
+  });
+
+  it("should return 'ancestor' if ", () => {
+    const fileSystem = new FileSystem();
+    expect(
+      getRelationship({
+        fileSystem,
+        kind: "file",
+        path: new List(["", "a"]),
+      }, {
+        fileSystem,
+        kind: "file",
+        path: new List([""]),
+      }),
+    ).toBe("ancestor");
+  });
+
+  it("should return 'other' if each path items is not same", () => {
+    const fileSystem = new FileSystem();
+    expect(
+      getRelationship({
+        fileSystem,
+        kind: "file",
+        path: new List(["a"]),
+      }, {
+        fileSystem,
+        kind: "file",
+        path: new List(["b"]),
+      }),
+    ).toBe("other");
+  });
+
+  it("should return 'direct child'", () => {
+    const fileSystem = new FileSystem();
+    expect(
+      getRelationship({
+        fileSystem,
+        kind: "file",
+        path: new List(["a"]),
+      }, {
+        fileSystem,
+        kind: "file",
+        path: new List(["a", "b"]),
+      }),
+    ).toBe("direct child");
+  });
+
+  it("should return 'descendant'", () => {
+    const fileSystem = new FileSystem();
+    expect(
+      getRelationship({
+        fileSystem,
+        kind: "file",
+        path: new List(["a"]),
+      }, {
+        fileSystem,
+        kind: "file",
+        path: new List(["a", "b", "c"]),
+      }),
+    ).toBe("descendant");
   });
 });
