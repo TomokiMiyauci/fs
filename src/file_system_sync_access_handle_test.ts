41318bcda09aec0c3301624056a2e4f7f7c9f98a
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-09-11 23:40:06 +0900

    test(file_system_sync_access_handle): add test case for FileSystemSyncAccessHandle

--- src/file_system_sync_access_handle_test.ts
+++ src/file_system_sync_access_handle_test.ts
@@ -1,5 +1,13 @@
+import { beforeEach, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
 import { runFileSystemSyncAccessHandleTest } from "@test/file_system_sync_access_handle.ts";
 import { VirtualFileSystem } from "@test/util.ts";
+import {
+  createNewFileSystemSyncAccessHandle,
+  FileSystemSyncAccessHandle,
+} from "./file_system_sync_access_handle.ts";
+import { FileEntry, FileSystem } from "@test/helper.ts";
+import { Msg } from "./constant.ts";
 
 runFileSystemSyncAccessHandleTest(async () => {
   const fs = new VirtualFileSystem();
@@ -7,4 +15,130 @@ runFileSystemSyncAccessHandleTest(async () => {
   return {
     root: await fs.getDirectory(),
   };
+});
+
+interface Context {
+  fileSystem: FileSystem;
+  fileEntry: FileEntry;
+  handle: FileSystemSyncAccessHandle;
+}
+
+describe("FileSystemSyncAccessHandle", () => {
+  beforeEach<Context>(function () {
+    this.fileSystem = new FileSystem();
+    this.fileEntry = new FileEntry(this.fileSystem);
+    this.handle = createNewFileSystemSyncAccessHandle(this.fileEntry);
+  });
+
+  describe("constructor", () => {
+    it("should not construct", () => {
+      // @ts-expect-error it should not construct
+      new FileSystemSyncAccessHandle();
+    });
+  });
+
+  describe("read", () => {
+    it<Context>("should throw error if state is not 'open'", function () {
+      this.handle["state"] = "close";
+
+      const u8 = new Uint8Array();
+      expect(() => this.handle.read(u8)).toThrow(
+        new DOMException(Msg.AlreadyClosed, "InvalidStateError"),
+      );
+    });
+
+    it<Context>("should return 0 if error has occurred", function () {
+      const u8 = new Uint8Array();
+
+      Object.defineProperty(this.fileEntry, "binaryData", {
+        get() {
+          throw new Error();
+        },
+      });
+
+      expect(this.handle.read(u8)).toBe(0);
+    });
+  });
+
+  describe("write", () => {
+    it<Context>("should throw error if state is not 'open'", function () {
+      this.handle["state"] = "close";
+
+      const u8 = new Uint8Array();
+      expect(() => this.handle.write(u8)).toThrow(
+        new DOMException(Msg.AlreadyClosed, "InvalidStateError"),
+      );
+    });
+
+    it<Context>("should throw error if error has occurred", function () {
+      const u8 = new Uint8Array();
+
+      Object.defineProperty(this.fileEntry, "binaryData", {
+        set() {
+          throw new Error();
+        },
+        get() {
+          return new Uint8Array();
+        },
+      });
+
+      expect(() => this.handle.write(u8)).toThrow(
+        new DOMException(Msg.InvalidOperation, "InvalidStateError"),
+      );
+    });
+  });
+
+  describe("truncate", () => {
+    it<Context>("should throw error if state is not 'open'", function () {
+      this.handle["state"] = "close";
+
+      expect(() => this.handle.truncate(0)).toThrow(
+        new DOMException(Msg.AlreadyClosed, "InvalidStateError"),
+      );
+    });
+
+    it<Context>("should throw error if error has occurred", function () {
+      Object.defineProperty(this.fileEntry, "binaryData", {
+        set() {
+          throw new Error();
+        },
+        get() {
+          return new Uint8Array(2);
+        },
+      });
+
+      expect(() => this.handle.truncate(1)).toThrow(
+        new DOMException(Msg.InvalidOperation, "InvalidStateError"),
+      );
+    });
+
+    it<Context>("should throw error if error has occurred 2", function () {
+      Object.defineProperty(this.fileEntry, "binaryData", {
+        set() {
+          throw new Error();
+        },
+        get() {
+          return new Uint8Array(2);
+        },
+      });
+
+      expect(() => this.handle.truncate(3)).toThrow(
+        new DOMException(Msg.InvalidOperation, "InvalidStateError"),
+      );
+    });
+  });
+
+  describe("getSize", () => {
+    it<Context>("should throw error if state is not 'open'", function () {
+      this.handle["state"] = "close";
+
+      expect(() => this.handle.getSize()).toThrow(
+        new DOMException(Msg.AlreadyClosed, "InvalidStateError"),
+      );
+    });
+
+    it<Context>("should return 0", function () {
+      expect(this.handle.getSize()).toBe(0);
+    });
+  });
 });
